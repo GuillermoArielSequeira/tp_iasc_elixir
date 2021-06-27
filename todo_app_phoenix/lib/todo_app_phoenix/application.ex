c6597cae0f3de1b416f61fbfce24a2ea30adc7b1
ralvarez <ralvarez@redhat.com>
2021-06-27 00:15:28 -0300

    test k8s

--- todo_app_phoenix/lib/todo_app_phoenix/application.ex
+++ todo_app_phoenix/lib/todo_app_phoenix/application.ex
@@ -2,13 +2,25 @@ defmodule TodoAppPhoenix.Application do
   use Application
 
   def start(_type, _args) do
+    topologies = [
+      example: [
+        strategy: Cluster.Strategy.Epmd,
+        config: [
+          hosts: [
+            :"node@iasc-elixir-0.iasc-elixir.default.svc.cluster.local",
+            :"node@iasc-elixir-1.iasc-elixir.default.svc.cluster.local",
+            :"node@iasc-elixir-2.iasc-elixir.default.svc.cluster.local"
+          ]
+        ]
+      ]
+    ]
     children = [
+      {Cluster.Supervisor, [topologies, [name: TodoAppPhoenix.ClusterSupervisor]]}
       TodoAppPhoenixWeb.Telemetry,
       {Phoenix.PubSub, name: TodoAppPhoenix.PubSub},
       TodoAppPhoenixWeb.Endpoint,
       Todo.System
     ]
-
     opts = [strategy: :one_for_one, name: TodoAppPhoenix.Supervisor]
     Supervisor.start_link(children, opts)
   end
