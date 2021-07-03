0dfae322aadf2885100d7c7b773cb4df52a66aac
Sebastian Chaves <seba.chaves.sc@gmail.com>
2021-07-03 11:05:44 -0300

    connect nodes

--- todo_app_phoenix/lib/todo_app_phoenix/application.ex
+++ todo_app_phoenix/lib/todo_app_phoenix/application.ex
@@ -3,10 +3,21 @@ defmodule TodoAppPhoenix.Application do
 
   def start(_type, _args) do
     children = [
+      # Start Cluster Supervisor
+      {Cluster.Supervisor, [topologies(), [name: TodoAppPhoenix.ClusterSupervisor]]},
+      # Start the Telemetry supervisor
       TodoAppPhoenixWeb.Telemetry,
+      # Start the PubSub system
       {Phoenix.PubSub, name: TodoAppPhoenix.PubSub},
-      TodoAppPhoenixWeb.Endpoint,
-      TodoAppPhoenix.Todo.System
+      # Start Horde
+      TodoAppPhoenix.HordeRegistry,
+      TodoAppPhoenix.HordeSupervisor,
+      TodoAppPhoenix.NodeObserver,
+      # Start Domain childrens
+      TodoAppPhoenix.Todo.System,
+
+      # Start the Endpoint (http/https)
+      TodoAppPhoenixWeb.Endpoint
     ]
 
     opts = [strategy: :one_for_one, name: TodoAppPhoenix.Supervisor]
@@ -16,5 +27,13 @@ defmodule TodoAppPhoenix.Application do
   def config_change(changed, _new, removed) do
     TodoAppPhoenixWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp topologies do
+    [
+      todo_app_phoenix: [
+        strategy: Cluster.Strategy.Gossip
+      ]
+    ]
   end
 end
--- todo_app_phoenix/mix.exs
+++ todo_app_phoenix/mix.exs
@@ -41,7 +41,9 @@ defmodule TodoAppPhoenix.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:libcluster, "~> 3.3"},
+      {:horde, "~> 0.8.4"}
     ]
   end
 
